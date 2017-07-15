4ec6380a4c7028a39a592cd001efddf0f22ea6e5
vagrant <vagrant@homestead>
2017-07-15 13:32:53 +0000

    Initial install and config Entrust, IDE-Helper, laravel-lang

--- config/app.php
+++ config/app.php
@@ -176,6 +176,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        /*
+         * Third-Party
+         */
+        Zizaco\Entrust\EntrustServiceProvider::class,
 
     ],
 
@@ -225,6 +230,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+       
+        //Third-Party
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 
